343f3042b2b7b6a77fee016e9a4d77f43a39a100
Zeynep Dönmez <84280282+zeynwpp@users.noreply.github.com>
2025-03-09 23:18:09 +0300

    Create analysing.sql

---
+++ analysing.sql
@@ -0,0 +1,22 @@
+--yıllara göre satış toplamı
+
+select s.YEAR_ID
+, sum(s.SALES) as toplam_satis
+from sales_data_sample s
+group by s.YEAR_ID
+
+--ay bazında ortalama sipariş tutarı
+
+select s.MONTH_ID
+, avg(s.QUANTITYORDERED) as AvgSales
+from sales_data_sample s
+group by s.MONTH_ID
+order by 1
+
+--en çok sipariş verilen 10 ürün kodu
+
+select top 10 s.productcode
+, sum(s.QUANTITYORDERED) as TotalQuantity
+from sales_data_sample s
+group by s.productcode
+order by TotalQuantity desc
